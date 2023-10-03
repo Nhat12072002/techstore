7845fece0b762a72837b45644f01c03c58eaebfc
Nhat12072002 <104749768+Nhat12072002@users.noreply.github.com>
2023-10-03 17:06:06 +0700

    fix bug category and extra images 3

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form.js
@@ -1,5 +1,39 @@
 
+$(document).ready(function() {
 
+	$("#shortDescription").richText();
+	$("#fullDescription").richText();
+
+	dropdownBrands.change(function() {
+		dropdownCategories.empty();
+		getCategories();
+	});
+
+	getCategoriesForNewForm();
+
+});
+
+function getCategoriesForNewForm() {
+	catIdField = $("#categoryId");
+	editMode = false;
+
+	if (catIdField.length) {
+		editMode = true;
+	}
+
+	if (!editMode) getCategories();
+}
+
+function getCategories() {
+	brandId = dropdownBrands.val();
+	url = brandModuleURL + "/" + brandId + "/categories";
+
+	$.get(url, function(responseJson) {
+		$.each(responseJson, function(index, category) {
+			$("<option>").val(category.id).text(category.name).appendTo(dropdownCategories);
+		});
+	});
+}
 $("#fileImage").change(function() {
 	if (!checkFileSize(this)) {
 		return;
